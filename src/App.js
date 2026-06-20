42a42b57ad0784b9fc08a132b727ec14fa9dffa1
Steffenkishore <kishoresteffen@gmail.com>
2026-06-20 16:59:38 +0530

    small changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@
 import './App.css';
 import TodoInput from './component/TodoInput';
 import TodoItems from './component/TodoItems';
+import { BiTask } from "react-icons/bi";
+<BiTask />
 
 
 function App() {
   return (
     <div className="todo-container">
-      <h1 className="todo-title">Todo App</h1>
+      <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
+        <BiTask style={{ alignSelf:"center", marginRight: "5px", fontSize:"28px"}} />
+        <h1 className="todo-title">Todo App</h1>
+      </div>
       <TodoInput />
       <TodoItems />
     </div>
